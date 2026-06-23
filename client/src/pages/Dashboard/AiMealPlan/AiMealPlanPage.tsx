2ae1dc66f390f377261590d41630d66ccce18f80
NZY1337 <mandreicosmin@yahoo.com>
2026-06-23 19:14:46 +0300

    tests added for Wizar (#11)
    Co-authored-by: Andrei Mocanu <andreimocanu@Andreis-MacBook-Air.local>

--- client/src/pages/Dashboard/AiMealPlan/AiMealPlanPage.tsx
+++ client/src/pages/Dashboard/AiMealPlan/AiMealPlanPage.tsx
@@ -43,7 +43,7 @@ export default function AiMealPlanPage() {
             <div className="space-y-6">
 
                 {/* Wizard */}
-                {showWizard && (
+                {true && (
                     <Wizard onGenerate={handleGenerate} isGenerating={isGenerating} />
                 )}
 
